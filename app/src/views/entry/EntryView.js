8cd96ce452d3f5ea31bf70718f822ba82608662d
Vishesh Duggar <vishesh@causecode.com>
2015-01-25 14:23:42 +0530

    Tweaks to the delete context menu #40

--- app/src/views/entry/EntryView.js
+++ app/src/views/entry/EntryView.js
@@ -34,16 +34,18 @@ define(function(require, exports, module) {
 
 		this.touchSync.on('start', function(data) {
 			this.start = Date.now();
+			// Show context menu after the timeout regardless of tap end
 			this.touchTimeout = setTimeout(function(){
 				App.pageView._eventOutput.emit('show-context-menu', { menu: this.menu, target: this, eventArg: this.entry});
-			}.bind(this),600)
+			}.bind(this),500)
 		}.bind(this));
 
 		this.touchSync.on('update', function(data) {
 			var movementX = Math.abs(data.position[0]);
 			var movementY = Math.abs(data.position[1]);
 			console.log('movementx: ', movementX, ' movementy: ', movementY);
-			if (movementX < 8 || movementY < 8) {
+			// Don't show context menu if there is intent to move something
+			if (movementX > 8 || movementY > 8) {
 				clearTimeout(this.touchTimeout);
 			}
 		}.bind(this));
@@ -53,6 +55,7 @@ define(function(require, exports, module) {
 			var movementX = Math.abs(data.position[0]);
 			var movementY = Math.abs(data.position[1]);
 			var timeDelta = this.end - this.start;
+			// If the intent is to just select don't show context menu
 			if (timeDelta < 500 && movementX < 8 && movementY < 8) {
 				clearTimeout(this.touchTimeout);
 				this._eventOutput.emit('select-entry', this.entry);
