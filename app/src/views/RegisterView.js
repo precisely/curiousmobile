8c9a8854403084aab8f52892928d7da7791bb58d
ravishankar <ravishankar.choudhary@causecode.com>
2015-12-17 14:28:31 +0530

    Added radio button for terms of services. #135

--- app/src/views/RegisterView.js
+++ app/src/views/RegisterView.js
@@ -77,7 +77,7 @@ define(function(require, exports, module) {
 		var confirmEmail = document.forms["registerForm"]["confirm_email"].value;
 		var username = document.forms["registerForm"]["username"].value;
 		var password = document.forms["registerForm"]["password"].value;
-        var terms    = document.getElementById("terms");
+        var terms    = document.getElementById("term-id").checked;
 
 		if (!email) {
 			u.showAlert("Email is a required field!");
@@ -91,8 +91,8 @@ define(function(require, exports, module) {
 			u.showAlert("Username is a required field!");
 		} else if (!password) {
 			u.showAlert("Password is a required field!");
-		} else if (!terms.value()=="aggrement") {
-            u.showAlert("You should agree on Terms and Conditions.");
+		} else if (!terms) {
+            u.showAlert("You should agree on Terms and Services.");
         } else {
 			user.register(
 				email,
